66d9742ad79097032fdddc4135248348d01f57b1
Ryan Weiss <rw3iss@gmail.com>
2020-07-26 18:33:09 -0400

    Updated readme

--- srr/modules/teeeest/teeeest.repository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { Teeeest } from './modeldir>teeeest.model';
-
-@EntityRepository(Teeeest)
-export class TeeeestRepository extends Repository<Teeeest> {
-
-}
